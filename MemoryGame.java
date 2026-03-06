28beded9dc1d6fe9cd8f6ce22e1a8a7e1cfd4589
Avinash Amanchi <148393523+avinashamanchi@users.noreply.github.com>
2026-03-06 10:49:48 -0800

    Update MemoryGame.java

--- MemoryGame.java
+++ MemoryGame.java
@@ -1,28 +1,42 @@
 /**
- * Project 3.1.5 Memory Game
- * * Logic: Displays a randomized sequence of strings. The player must 
- * recreate the sequence from memory.
+ * Project 3.1.5
+ *
+ * The Memory Game shows a random sequence of "memory strings" in a variety of buttons.
+ * After wathcing the memory strings appear in the buttons one at a time, the
+ * player recreates the sequence from memory.
  */
 public class MemoryGame
 {
   public static void main(String[] args) {
 
-    // TO DO: Create the "memory strings" - an array of 5 single character strings
+    // TO DO: Create the "memory strings" - an array of at least 4 single character  
+    // strings to show in the buttons, one element at a time. This is the sequence
+    // the player will have to remember.
+
+    // Create the game and gameboard. Configure a randomized board with 3 buttons.
+    // (Later, you can change options to configure more or less buttons
+    // and turn randomization on or off.))
+
     String[] memoryStrings = {"A", "B", "C", "D", "E"};
 
-    // Create the game and gameboard. Configure with 3 buttons.
     MemoryGameGUI game = new MemoryGameGUI();
     game.createBoard(3, true);
 
-    // Initialize scoring and loop variables
     int score = 0;
     int rounds = 0;
     boolean keepPlaying = true;
 
+        // TO DO: Play the game until user wants to quit.
+  
+        // TO DO: Call the next method in RandomPermutation to create a random version 
+        // of the "memory strings"
+  
+        // TO DO: Play one sequence with a .5 second delay. Save the player's guess. 
+        // (Later, you can speed up or slow down the game.)
 
     // role switch
-    // Hrihaan: Driver (person writing the code)
-    // Avi: Navigator (suggesting edits and guiding)
+    // Hrihaan (person writing the code)
+    // Avi (suggesting edits and guiding)
 
     // main game loop runs until the player decides to stop
     while (keepPlaying) {
@@ -44,10 +58,19 @@ public class MemoryGame
         // the player's input guess is captured 
         String guess = game.playSequence(randomSeq, 5);
 
+            // TO DO: If the gess is not null, determine a match
+        
+            // TO DO: Cleanup the guess - repalce commas and spaces with the empty string.
+            // Refer to a new String method replace.
+            
+            // TO DO: Iterate to determine if all elements of the guess match sequence
+    
+            // If match, signal a match, otherwise, try again.
+
 
         // role switch #2
-        // Avi: Driver 
-        // Hrihaan: Navigator
+        // Avi main coder 
+        // Hrihaan edits
 
         // check that the guess is not null (handles the Cancel button case)
         if (guess != null) {
@@ -72,8 +95,8 @@ public class MemoryGame
 
 
         // role switch #3
-        // Hrihaan: Driver
-        // Avi: Navigator
+        // Hrihaan code driver
+        // Avi edit navigator
 
         // ask the player if they want to keep playing
         keepPlaying = game.playAgain();
